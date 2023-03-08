cfec87844092d961931adf20ca5e3069bf50eb92
baozgen <baris.ozgen@kellogg.northwestern.edu>
2023-03-08 16:20:51 +0000

    adds authentication code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,24 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # login the user
+        flash["notice"] = "You've logged in."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash["notice"] = "Unsuccessful login."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
   end
+
 end
   
